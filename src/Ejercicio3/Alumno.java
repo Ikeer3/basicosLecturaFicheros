96fef3c37cd217d75cd332f868d57963ffd296a4
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-04-28 09:29:26 +0200

    Añadidos atributos y getters

---
+++ src/Ejercicio3/Alumno.java
@@ -0,0 +1,24 @@
+package Ejercicio3;
+
+public class Alumno {
+
+    private String nombre;
+    private int nota;
+
+    public Alumno(String nombre, int nota) {
+        this.nombre = nombre;
+        this.nota = nota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void mostrarDatos() {
+        System.out.print("\n" + nombre + " ha sacado un " + nota);
+    }
+}
